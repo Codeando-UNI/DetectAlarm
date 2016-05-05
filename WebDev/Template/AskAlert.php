1f4f67635d231b488037ca4235ff08d8ed538633
JOTAI <juan.basflo.initec@gmail.com>
2016-05-05 18:08:02 -0500

    ultimos cambios, funciona bien

--- WebDev/Template/AskAlert.php
+++ WebDev/Template/AskAlert.php
@@ -2,6 +2,9 @@
 require_once ("../require/conexion_class.php");
 require_once ("../require/Alerta_class.php");
 
+// URL
+// GET estacion.waposat.com/Public_html/Template/AskAlert.php?equipo=20
+
 $id_equipo = htmlspecialchars($_GET["equipo"],ENT_QUOTES);
 
 //echo "se han leido los valores";
